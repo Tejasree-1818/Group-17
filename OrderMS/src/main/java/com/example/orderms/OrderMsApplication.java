b4aeae90f2016c0adbba8587b871078e81f4e288
Teja <venkata.desamsetty@infosys.com>
2021-05-18 18:36:38 +0530

    Updated OrderMS

--- OrderMS/src/main/java/com/example/orderms/OrderMsApplication.java
+++ OrderMS/src/main/java/com/example/orderms/OrderMsApplication.java
@@ -4,14 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-
 public class OrderMsApplication 
 {
-
-
-
-
-	public static void main(String[] args) {
+        public static void main(String[] args) {
 		SpringApplication.run(OrderMsApplication.class, args);
 	}
 
