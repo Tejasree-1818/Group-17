773252114ac83c5b393eb712444c807e490fb576
Rakshita <Rakshita@LAPTOP-75JKPQFP>
2021-05-18 12:54:41 +0530

    changes made in dto

---
+++ UserMS/src/main/java/com/example/dto/SellerDTO.java
@@ -0,0 +1,81 @@
+package com.example.dto;
+
+import com.example.entity.Seller;
+
+public class SellerDTO {
+	
+	private Integer sellerId;
+	private String name;
+	private String emailId;
+	private long phoneNumber;
+	private String password;
+	private Boolean isActive;
+	public Integer getSellerId() {
+		return sellerId;
+	}
+	public void setSellerId(Integer sellerId) {
+		this.sellerId = sellerId;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmailId() {
+		return emailId;
+	}
+	public void setEmailId(String emailId) {
+		this.emailId = emailId;
+	}
+	public long getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(long phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public Boolean getIsActive() {
+		return isActive;
+	}
+	public void setIsActive(Boolean isActive) {
+		this.isActive = isActive;
+	}
+	
+	@Override
+	public int hashCode()
+	{
+	final int prime=31;
+	int result=1;
+	result=prime * result +((sellerId==null) ? 0: sellerId.hashCode());
+	return result;
+	}
+	
+	
+	@Override
+	  public boolean equals(Object obj)
+	  {
+	      if(this ==obj)
+	         return true;
+	      if(obj==null)
+	         return false;
+	      if(getClass()!=obj.getClass())
+	         return false;
+	      SellerDTO sDTO= (SellerDTO) obj;
+	      if(sellerId==null){
+	         if(sDTO.sellerId != null)
+	              return false;
+	      }
+	      else if (!sellerId.equals(sDTO.sellerId))
+	               return false;
+	      return true;
+
+	}
+
+
+}
--- UserMS/src/main/java/com/example/dto/WishlistDTO.java
+++ UserMS/src/main/java/com/example/dto/WishlistDTO.java
@@ -2,21 +2,17 @@ package com.example.dto;
 
 import com.example.entity.Buyer;
 
-public class cartDTO {
+public class WishlistDTO {
 	
 	private Buyer buyerId;
-	private Integer quantity;
+
 	public Buyer getBuyerId() {
 		return buyerId;
 	}
+
 	public void setBuyerId(Buyer buyerId) {
 		this.buyerId = buyerId;
 	}
-	public Integer getQuantity() {
-		return quantity;
-	}
-	public void setQuantity(Integer quantity) {
-		this.quantity = quantity;
-	}
+	
 
 }
