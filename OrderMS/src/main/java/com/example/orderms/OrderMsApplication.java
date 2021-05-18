34c5a7b74d9f31a5d1fb0b7296df469c91c4d078
Rakshita <Rakshita@LAPTOP-75JKPQFP>
2021-05-18 10:53:04 +0530

    commited

--- OrderMS/src/main/java/com/example/orderms/OrderMsApplication.java
+++ OrderMS/src/main/java/com/example/orderms/OrderMsApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class OrderMsApplication {
-
+//akshita
 	public static void main(String[] args) {
 		SpringApplication.run(OrderMsApplication.class, args);
 	}
